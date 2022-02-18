798d538a23a5d013899ed41339c8cc1b97ec7e54
Number5IsAlive <james.s.mason@hotmail.com>
2022-02-18 14:08:38 +0000

    3 Working on movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float velocity = 3.0f;
+    [SerializeField] float jumpVelocity = 3.0f;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody = null;
+
+    bool isJumping = false;
+
+    void Awake()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        Debug.Log("Hello");
+        if (!isJumping)
+        {
+            myRigidbody.AddForce(Vector2.up * jumpVelocity);
+            isJumping = true;
+            return;
+        }
+        isJumping = false;
+    }
+
+    private void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * velocity, 0);
+        myRigidbody.velocity = playerVelocity;
     }
 }
